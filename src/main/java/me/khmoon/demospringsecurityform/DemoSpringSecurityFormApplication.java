041754ea3ed855236c80e8e597049b4b4738456f
robin <mgh3326@naver.com>
2020-03-08 23:48:43 +0900

    스프링 시큐리티 커스터마이징

--- src/main/java/me/khmoon/demospringsecurityform/DemoSpringSecurityFormApplication.java
+++ src/main/java/me/khmoon/demospringsecurityform/DemoSpringSecurityFormApplication.java
@@ -2,6 +2,9 @@ package me.khmoon.demospringsecurityform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class DemoSpringSecurityFormApplication {
@@ -10,4 +13,8 @@ public class DemoSpringSecurityFormApplication {
     SpringApplication.run(DemoSpringSecurityFormApplication.class, args);
   }
 
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
 }
